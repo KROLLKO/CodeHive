fc3bf471ef60ce1ed4a8b2efea12765e30fd6134
Ingrid Sastoque <inyasaco2010@hotmail.com>
2023-07-10 06:47:48 -0500

    Entrega final

--- js/buscador-libros.js
+++ js/buscador-libros.js
@@ -13,32 +13,38 @@ const searchBooks = (event) => {
 
   const url = `https://openlibrary.org/search.json?title=${encodeURIComponent(searchTerm)}`;
 
+  // Mostramos el mensaje de "Estamos buscando..."
+  bookContainer.innerHTML = '<p>Estamos buscando...</p>';
+
   // Utilizamos la función fetch para obtener los datos de la API
   fetch(url)
     .then(response => response.json()) // Convertimos la respuesta a JSON
     .then(data => {
       bookContainer.innerHTML = ""; // Limpiamos el contenedor de libros
 
-      // Recorremos los resultados y creamos elementos HTML para cada libro
-      data.docs.slice(0, 5).forEach(book => {
-        const bookElement = document.createElement("div");
-        bookElement.classList.add("book");
-        bookElement.innerHTML = `
-          <h3>${book.title}</h3>
-          <p>Autor: ${book.author_name ? book.author_name.join(", ") : "Desconocido"}</p>
-          <p>Año de publicación: ${book.first_publish_year || "Desconocido"}</p>
-          <button>Ver más</button>
-          <hr>
-        `;
-
-        // Agregamos el evento de clic para redirigir al libro
-        bookElement.addEventListener("click", () => {
-          window.location.href = `https://openlibrary.org${book.key}`;
+      if (data.docs.length === 0) {
+        // No se encontraron resultados
+        const noResultsElement = document.createElement("div");
+        noResultsElement.classList.add("no-results");
+        noResultsElement.innerText = "No se han encontrado recursos.";
+        bookContainer.appendChild(noResultsElement);
+      } else {
+        // Recorremos los resultados y creamos elementos HTML para cada libro
+        data.docs.slice(0, 5).forEach(book => {
+          const bookElement = document.createElement("div");
+          bookElement.classList.add("book");
+          bookElement.innerHTML = `
+            <h3>${book.title}</h3>
+            <p>Autor: ${book.author_name ? book.author_name.join(", ") : "Desconocido"}</p>
+            <p>Año de publicación: ${book.first_publish_year || "Desconocido"}</p>
+            <button onclick="openBook('https://openlibrary.org${book.key}')">Ver más</button>
+            <hr>
+          `;
+  
+          // Agregamos el elemento del libro al contenedor
+          bookContainer.appendChild(bookElement);
         });
-
-        // Agregamos el elemento del libro al contenedor
-        bookContainer.appendChild(bookElement);
-      });
+      }
     })
     .catch(error => {
       console.error("Error:", error);
@@ -48,5 +54,17 @@ const searchBooks = (event) => {
     });
 };
 
+// Función para abrir el enlace del libro en una nueva pestaña
+function openBook(url) {
+  window.open(url, '_blank');
+}
+
 // Escuchamos el evento de envío del formulario
 searchForm.addEventListener("submit", searchBooks);
+
+// Escuchamos el evento de presionar la tecla "Enter" en el campo de entrada de búsqueda
+searchInput.addEventListener("keydown", event => {
+  if (event.key === "Enter") {
+    searchBooks(event);
+  }
+});
